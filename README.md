f25d10fc642658d6dc19f412b1d39a506e967c0c
seansamu <seansamu.code@gmail.com>
2025-02-05 09:11:30 -0500

    created base files/folders and added endpoint

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# PetCo
+# Smart Pet Collar
+
+< Insert Description >
+
+### Instructions
+
+download dependencies
+
+cd src
+
+node app.js
+
+go to localhost:3000 for main page
+
+to post, have packet sent in using the example in index.html (src/public)
--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require("express")
+const parser = require("body-parser")
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send("hello world")
-})
+let jsonParser = parser.json()
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(port, () => {console.log(`listening on port ${port}`)})
+app.use(express.static('public'))
+
+app.post('/api', jsonParser, (req, res) => {
+    console.log(req.body)
+    res.send({ status : "SUCCESS"})
 })
