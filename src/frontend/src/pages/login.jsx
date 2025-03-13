decd4eb0214db2c4e027b64115dd2c5d65a4683d
seansamu <seansamu.code@gmail.com>
2025-03-13 18:16:05 -0400

    finished basic aesthetic

--- src/frontend/src/pages/login.jsx
+++ src/frontend/src/pages/login.jsx
@@ -1,13 +1,14 @@
-import { Container, Flex, Text, Box, Textarea, Checkbox, Button } from '@chakra-ui/react'
+import { Container, Flex, Text, Box, Textarea, Checkbox, Button, Group } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
-    return <Container maxW={"100%"} px={4} paddingTop={3}>
+    return <Container maxW={"100%"} px={4} paddingTop={3} color={"white"}>
             <Link to={"/"}>
                 <Text
                     ml={2}
                     fontSize={"32px"}
                     fontWeight={"bolder"}
+                    letterSpacing={"wide"}
                 >SPC</Text>
             </Link>
 
@@ -20,27 +21,79 @@ const Login = () => {
                     w={"60%"}
                     bg={"#b6c4a2"}
                     rounded={"md"}
+                    shadow={"5px 5px 8px 1px rgba(0, 0, 0, 0.2)"}
                 >
                     <Flex
-                    
+                        justifyContent={"center"}
+                        flexDir={"column"}
                     >
-                        <Text>Username:</Text>
-                        <Textarea resize={"none"}></Textarea>
-                        <Text>Password:</Text>
-                        <Textarea resize={"none"}></Textarea>
+                        <Group
+                            w={"65%"}
+                            mt={"10%"}
+                            ml={"15%"}
+                        >
+                            <Flex
+                                flexDir={"column"}
+                            >
+                                <Text>Username:</Text>
+                                <Textarea 
+                                    w={"710px"}
+                                    h={"30px"}
+                                    resize={"none"} 
+                                    bg={"white"} 
+                                    border={"none"}
+                                    color={"black"}
+                                    rounded={"0"}
+                                ></Textarea>
+                            </Flex>
+                        </Group>
+                        <Group
+                            w={"65%"}
+                            mt={"5%"}
+                            ml={"15%"}
+                        >
+                            <Flex
+                                flexDir={"column"}
+                            >
+                                <Text textAlign={"left"}>Password:</Text>
+                                <Textarea 
+                                    w={"710px"}
+                                    h={"30px"}
+                                    resize={"none"} 
+                                    bg={"white"} 
+                                    border={"none"}
+                                    color={"black"}
+                                    rounded={"0"}
+                                ></Textarea>
+                            </Flex>
+                        </Group>
 
-                        <Checkbox.Root>
-                            <Checkbox.HiddenInput />
-                            <Checkbox.Control>
-                                <Checkbox.Indicator />
-                            </Checkbox.Control>
-                            <Checkbox.Label>Administrator</Checkbox.Label>
-                        </Checkbox.Root>
-                        <Link to={"/signup"}>
-                            <Text textDecoration={"underline"}>Sign up?</Text>
-                        </Link>
+                        <Container mt={"2"}>
+                            <Flex
+                                mt={"1%"}
+                                ml={"12.8%"}
+                                gap={"33rem"}
+                                justifyContent={"left"}
+                                flexDir={"row"}
+                            >
+                                <Checkbox.Root>
+                                    <Checkbox.HiddenInput />
+                                    <Checkbox.Control>
+                                        <Checkbox.Indicator />
+                                    </Checkbox.Control>
+                                    <Checkbox.Label>Administrator</Checkbox.Label>
+                                </Checkbox.Root>
+                                <Link to={"/signup"}>
+                                    <Text textDecoration={"underline"}>Sign up?</Text>
+                                </Link>
+                            </Flex>
+                        </Container>
 
-                        <Button>Login</Button>
+                        <Button
+                            w={"10%"}
+                            mt={"5%"}
+                            ml={"44%"}
+                        >Login</Button>
                     </Flex>
                 </Box>
             </Flex>
